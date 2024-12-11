8d323eb29d403f14a35895c5aba48797099f6dd8
Joel Kagohara Sasaki <126785821+JoelKSasaki@users.noreply.github.com>
2024-12-11 19:49:25 +0000

    brasileirão

---
+++ projeto_tds/templates/scripts/index.js
@@ -0,0 +1,15 @@
+const brasileirao = "https://api.api-futebol.com.br/v1/campeonatos/10";
+
+//pegar os jogos
+async function GetGames(){
+
+    const resposta = await fetch(brasileirao);
+
+    console.log(resposta);
+
+    const jogos = await resposta.json();
+
+    console.log(jogos);
+}
+
+GetGames();
